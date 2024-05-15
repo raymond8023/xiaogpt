1c231a97ed01a8b63e7250207942e5f10e6db313
raymond8023 <117613639+raymond8023@users.noreply.github.com>
2024-05-15 09:00:05 +0800

    Update config.py

--- xiaogpt/config.py
+++ xiaogpt/config.py
@@ -37,7 +37,7 @@ HARDWARE_COMMAND_DICT = {
 
 DEFAULT_COMMAND = ("5-1", "5-5")
 
-KEY_WORD = ("帮我", "请")
+KEY_WORD = ("请帮我问")
 CHANGE_PROMPT_KEY_WORD = ("更改提示词",)
 PROMPT = "以下请用300字以内回答，请只回答文字不要带链接"
 # simulate_xiaoai_question
